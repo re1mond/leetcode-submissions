0b4e04166e5676b2d63785fb14dad3ed7ae170a6
unknown <re1mond@outlook.com>
2021-07-19 22:29:29 +0300

    initial

---
+++ algorithms/Lowest Common Ancestor of a Binary Search Tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
+const lowestCommonAncestor = (root, p, q) => {
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+
+  return root;
+};
